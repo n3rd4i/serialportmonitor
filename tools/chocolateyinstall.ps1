c7d141537007abe34465b7b7cdbf140121367ccb
Andrei <n3rd4i@gmail.com>
2019-09-02 19:17:20 +0200

    update to 1.2.1

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,17 +1,16 @@
 ﻿$ErrorActionPreference = 'Stop'; # stop on all errors
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://netix.dl.sourceforge.net/project/serial-port-monitor/Serial%20Port%20Monitor%20v1.1.2/Serial%20Port%20Monitor%20v1.1.2.exe' # download url, HTTPS preferred
+$url        = 'https://downloads.sourceforge.net/project/serial-port-monitor/Serial%20Port%20Monitor%20v1.2.1/Serial%20Port%20Notifier%20v1.2.1.exe' # download url, HTTPS preferred
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $toolsDir
-  fileType      = 'exe' #only one of these: exe, msi, msu
+  fileType      = 'exe'
   url           = $url
-  softwareName  = 'serialportmonitor*' #part or all of the Display Name as you see it in Programs and Features. It should be enough to be unique
-  checksum      = '0FFA5D41862094CCA7AF59F169C968BF'
-  checksumType  = 'md5' #default is md5, can also be sha1, sha256 or sha512
+  softwareName  = 'serialportmonitor*'
+  checksum      = '451B3C8124CDF6F95E85236DF55C9D7B972E6800C99E2B6235B2DA4CC137D049'
+  checksumType  = 'sha256'
   validExitCodes= @(0, 3010, 1641)
   silentArgs   = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-' # Inno Setup
 }
 
-Install-ChocolateyPackage @packageArgs # https://chocolatey.org/docs/helpers-install-chocolatey-package
-
+Install-ChocolateyPackage @packageArgs
