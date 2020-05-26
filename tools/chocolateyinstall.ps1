7e0ac6c9438fe760509bfb6901831e6943dc6183
Andrei Hutanu <andrei.hutanu@evalan.com>
2020-05-26 16:20:02 +0200

    updated to 1.2.2

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,12 +1,12 @@
 ﻿$ErrorActionPreference = 'Stop'; # stop on all errors
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://downloads.sourceforge.net/project/serial-port-monitor/Serial%20Port%20Monitor%20v1.2.1/Serial%20Port%20Notifier%20v1.2.1.exe' # download url, HTTPS preferred
+$url        = 'https://downloads.sourceforge.net/project/serial-port-monitor/Serial%20Port%20Monitor%20v1.2.2/Serial%20Port%20Notifier%20v1.2.2.exe' # download url, HTTPS preferred
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   fileType      = 'exe'
   url           = $url
   softwareName  = 'serialportmonitor*'
-  checksum      = '451B3C8124CDF6F95E85236DF55C9D7B972E6800C99E2B6235B2DA4CC137D049'
+  checksum      = 'DC808383C773152DD14E2EC1394BE4D5F3302B40CE906004892E06A7F3E5F15D'
   checksumType  = 'sha256'
   validExitCodes= @(0, 3010, 1641)
   silentArgs   = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-' # Inno Setup
